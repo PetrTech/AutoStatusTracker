1f7fefcf7872fcd930ff85dc753c923b4c4277a6
PetrTech <petrne09@gmail.com>
2024-07-28 12:20:07 +0200

    Finished v1.0

--- public/behavior.js
+++ public/behavior.js
@@ -1,3 +1,4 @@
+// Keep only 3
 var statusDisplayNames = {
     "ONLINE": "ONLINE",
     "UNEXPECTED BEHAVIOR": "UNEXPECTED BEHAVIOR",
@@ -10,6 +11,15 @@ var statusClassNames = {
     "OFFLINE": "statusNegative"
 };
 
+// Keep only 4
+var mainStatusNames = [
+    "NOMINAL",
+    "ACCEPTABLE", // If everything is ONLINE and there are one or more unexpected behaviors
+    "PARTIAL OUTAGE",
+    "BIG OUTAGE",
+    "TOTAL OUTAGE"
+]
+
 function httpGet(url){
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", url, false);
@@ -17,6 +27,9 @@ function httpGet(url){
     return xmlHttp.responseText;
 }
 
+var offlineServices = 0;
+var totalServices = 0;
+var unexpectedResults = 0;
 async function searchChild(object, parent) {
     if (!object) { return; }
 
@@ -79,6 +92,16 @@ async function searchChild(object, parent) {
             el.appendChild(statusSpan)
             el.appendChild(span);
             parent.appendChild(el);
+
+            if(object["STATUS"] == "OFFLINE"){
+                offlineServices += 1;
+            }
+            if(object["STATUS"] == "UNEXPECTED BEHAVIOR"){
+                unexpectedResults += 1;
+            }
+
+            totalServices += 1;
+
             break;
         
         // Unrecognized target types
@@ -86,23 +109,59 @@ async function searchChild(object, parent) {
             console.warn("UNRECOGNIZED OBJECT TYPE IN CONFIGURATION");
             return;
     }
+
+    checkStatus(offlineServices, totalServices, unexpectedResults);
 }
 
 var endpoints = JSON.parse(httpGet(window.location.origin + "/getEndpoints"));
 searchChild(endpoints.targets, document.getElementById("container"));
 
+// AUTOMATIC TIMED UPDATES
 async function updateEndpoints(){
+    var offlineServices = 0;
+    var totalServices = 0;
+    var unexpectedServices = 0;
+
     var endpoints = JSON.parse(httpGet(window.location.origin + "/getStatus"));
 
+    totalServices = endpoints.length;
+
     for(var endpoint of endpoints){
+        if(endpoint["STATUS"] == "OFFLINE"){
+            offlineServices += 1;
+        }
+        if(endpoint["STATUS"] == "UNEXPECTED BEHAVIOR"){
+            unexpectedServices += 1;
+        }
+
         var update = document.getElementById(endpoint["assignedId"]);
         update.innerHTML = statusDisplayNames[endpoint["STATUS"]];
         update.className = '';
         update.classList.add("endpointStatus");
         update.classList.add(statusClassNames[endpoint["STATUS"]]);
     }
+
+    checkStatus(offlineServices, totalServices, unexpectedServices);
+}
+
+function checkStatus(offline, total, unexpected){
+    var severity = 0;
+    
+    if(offline > 0){
+        severity = 2
+    }else if(offline >= Math.floor(total/2)){
+        severity = 3
+    }else if(offline >= total){
+        severity = 4
+    }
+
+    if(severity == 0 && unexpected > 0){
+        severity = 1;
+    }
+
+    document.getElementById("mainstatus").innerText = mainStatusNames[severity];
 }
 
 setInterval(async () => {
     await updateEndpoints();
-}, 1 * 1000);
+}, 30 * 1000);
